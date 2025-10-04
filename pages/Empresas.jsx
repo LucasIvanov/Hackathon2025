264bdfe2c9bfe8c8eb4ec34a9dd4519f156a0398
Vinicius Eduardo <114528382+edu-morais1@users.noreply.github.com>
2025-10-04 13:49:43 -0300

    Refatorar views para usar serviços e melhorar organização

---
+++ pages/Empresas.jsx
@@ -0,0 +1,36 @@
+import { useEffect, useState } from "react";
+import api from "../api/axios.jsx";
+
+export default function Empresas() {
+  const [empresas, setEmpresas] = useState([]);
+
+  useEffect(() => {
+    api.get("/empresas/").then(res => setEmpresas(res.data.results));
+  }, []);
+
+  return (
+    <div style={{ margin: 40 }}>
+      <h2>Empresas</h2>
+      <table border={1} cellPadding={5}>
+        <thead>
+          <tr>
+            <th>CNPJ</th>
+            <th>Razão Social</th>
+            <th>Bairro</th>
+            <th>Porte</th>
+          </tr>
+        </thead>
+        <tbody>
+          {empresas.map(emp => (
+            <tr key={emp.cnpj}>
+              <td>{emp.cnpj}</td>
+              <td>{emp.razao_social}</td>
+              <td>{emp.bairro}</td>
+              <td>{emp.porte}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
